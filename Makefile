2b063587fc9d2824a571aa22f0fbabffd70a2d0b
aoi-shanyrak <maxim.mezheniy@gmail.com>
2026-05-24 20:27:08 +0700

    done

--- Makefile
+++ Makefile
@@ -4,17 +4,18 @@ boot.bin: boot.asm
 	nasm -fbin boot.asm -o boot.bin
 
 boot.img: boot.bin
-	dd if=/dev/zero of=boot.img bs=1024 count=1440  
-	dd if=boot.bin of=boot.img conv=notrunc         
+	dd if=/dev/zero of=boot.img bs=512 count=2880
+	dd if=boot.bin of=boot.img conv=notrunc
+	dd if=aoi.txt of=boot.img conv=notrunc seek=1
 
 run: boot.img
 	qemu-system-i386 -cpu pentium2 -m 1g -fda boot.img -monitor stdio -device VGA
 
 clean:
-	rm -f boot.bin boot.img
+	rm -f boot.bin boot.img aoi.bin
 
 debug: boot.img
-	qemu-system-i386 -cpu pentium2 -m 1g -fda boot.img -monitor stdio -device VGA &
+	qemu-system-i386 -cpu pentium2 -m 1g -fda boot.img -monitor stdio -device VGA -s -S &
 	gdb -ex "target remote localhost:1234" -ex "break *0x7C00" -ex "continue"
 
 .PHONY: all run clean debug
--- boot.asm
+++ boot.asm
@@ -2,6 +2,10 @@
 [ORG 0x7C00]
 
 
+%define SECTOR_COUNT     2
+%define KERNEL_LOAD_ADDR 0x7E00
+
+
 start:
     cli
     xor ax, ax
@@ -11,26 +15,44 @@ start:
     mov sp, 0x7C00
     sti
 
+
+load_kernel:
+    mov ah, 0x02
+    mov al, SECTOR_COUNT
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
+    mov bx, KERNEL_LOAD_ADDR
+    int 0x13
+    jc  disk_error
     
 hello:
     mov si, hello_msg
-    call print_string
-
+    call    print_string
 
 end:
-    jmp $
+    jmp end
+
+
+disk_error:
+    mov si, disk_error_msg
+    call    print_string
+    jmp     end
 
 
 print_string:
     lodsb
-    test al, al
-    jz .done
-    mov ah, 0x0E
-    int 0x10
-    jmp print_string
+    test  al, al
+    jz    .done
+    mov   ah, 0x0E
+    int   0x10
+    jmp   print_string
 .done:
     ret
 
+
+disk_error_msg:
+    db 'Disk read error!', 0x0A, 0x0D, 0
 
 hello_msg: 
     db 'Hello, World!', 0x0A, 0x0D, 0
